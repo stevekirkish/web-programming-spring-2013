c3dbb6def651a855f2829363ce6b24601f18a3c7
Steve Kirkish <sdkirkish@yahoo.com>
2013-01-30 19:47:04 -0800

    Index.php: Changed fav_icon image

--- lesson-02/index.php
+++ lesson-02/index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title>CS 55.11: Steve Kirkish Homepage</title>
-<link rel="icon" href="images/js_icon.ico" type="image/x-icon">
+<link rel="icon" href="images/favicon-hw.ico" type="image/x-icon">
 <link rel="stylesheet" href="css/main_css.css" type="text/css" media="all">
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
